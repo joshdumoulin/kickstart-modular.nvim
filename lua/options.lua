14d5ea9c92a677beb9b286bbb1b0d2874ed06aff
joshdumoulin <j.schweikart.dumoulin@student.tue.nl>
2025-06-18 23:39:27 +0200

    Open file at the last position it was edited earlier

--- lua/options.lua
+++ lua/options.lua
@@ -96,3 +96,10 @@ vim.cmd [[autocmd FileType * set formatoptions-=ro]]
 
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Open file at the last position it was edited earlier',
+  group = misc_augroup,
+  pattern = '*',
+  command = 'silent! normal! g`"zv',
+})
